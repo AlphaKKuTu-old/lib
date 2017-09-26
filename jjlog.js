332b09059ba8810ce53ac8225f97bca0c92b657d
small_sunshine <jioo0224@naver.com>
2017-09-26 18:37:46 +0900

    첫 커밋
    파일 추가/첫 커밋

---
+++ jjlog.js
@@ -0,0 +1,63 @@
+/**
+ * Rule the words! KKuTu Online
+ * Copyright (C) 2017 JJoriping(op@jjo.kr)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+  
+/**
+ * 볕뉘 수정사항:
+ * var 에서 let/const 로 변수 선언자 변경
+ */
+
+// 모듈 호출
+
+const colors = require('colors');
+
+function callLog(text){
+	let date = new Date();
+	let o = {
+		year: 1900 + date.getYear(),
+		month: date.getMonth() + 1,
+		date: date.getDate(),
+		hour: date.getHours(),
+		minute: date.getMinutes(),
+		second: date.getSeconds()
+	}, i;
+	
+	for(i in o){
+		if(o[i] < 10) o[i] = "0"+o[i];
+		else o[i] = o[i].toString();
+	}
+	console.log("["+o.year+"-"+o.month+"-"+o.date+" "+o.hour+":"+o.minute+":"+o.second+"] "+text);
+}
+exports.log = function(text){
+	callLog(text);
+};
+exports.info = function(text){
+	callLog(text.cyan);
+};
+exports.success = function(text){
+	callLog(text.green);
+};
+exports.alert = function(text){
+	callLog(text.yellow);
+};
+exports.warn = function(text){
+	callLog(text.black.bgYellow);
+};
+exports.error = function(text){
+	callLog(text.bgRed);
+};
