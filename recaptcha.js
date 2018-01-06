a767a88c3f658cc8e0e86cd746f098d6fd1de614
small_sunshine <jioo0224@naver.com>
2018-01-06 13:38:51 +0900

    config 관련 수정

--- recaptcha.js
+++ recaptcha.js
@@ -2,7 +2,7 @@
  * Created by horyu1234 on 2017-11-14.
  */
 const request = require('request');
-const GLOBAL = require("./global.json");
+const GLOBAL = require("../../global.json");
 
 exports.verifyRecaptcha = function (responseToken, remoteIp, callback) {
     const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${GLOBAL.GOOGLE_RECAPTCHA_SECRET_KEY}&response=${responseToken}&remoteip=${remoteIp}`;
